e7dbf6558c0c1244f3a8a10694eb5fac2ea2a0ff
PurpleM1st <bogdancostaspreda@gmail.com>
2026-08-04 11:44:22 +0300

    added startup pipeline

---
+++ main.py
@@ -0,0 +1,30 @@
+import os
+import subprocess
+import sys
+import time
+
+def run_code(code_name):
+	os.makedirs("logs", exist_ok=True)
+	name_without_py = os.path.splitext(code_name)[0]
+	path = os.path.join("logs", f"{name_without_py}.log")
+
+	with open(path, 'w', encoding="utf-8") as file_log:
+		did_run = subprocess.run([sys.executable, code_name], 
+						   stdout=file_log, stderr=subprocess.STDOUT)
+
+	if did_run.returncode != 0:
+		print("Something went wrong")
+		sys.exit(did_run.returncode)
+	else:
+		print(f"{code_name} has run succesfully")
+	
+
+if __name__ == "__main__":
+	time_start = time.time()
+
+	pipeline = ["model.py"]
+	for code in pipeline:
+		run_code(code)
+
+	duration = time.time() - time_start
+	print(f"The runtime lasted {duration:.2f} seconds")
