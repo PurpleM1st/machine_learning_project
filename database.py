1329df7ebfc0640d71f2c924ba31cb9fc337300d
PurpleM1st <bogdancostaspreda@gmail.com>
2026-08-05 13:00:13 +0300

    changed the name of the validation folder and changed the logic of the database creation

--- database.py
+++ database.py
@@ -1,48 +1,38 @@
 import numpy as np
-import pandas as pd
-import torch
-import timm
 import os
 import cv2
-import subprocess
-import sys
 
 CACHE = "cached_data.npz"
 
-def get_data():
-	if os.path.exists(CACHE):
-		print(f"Loading data from cache: {CACHE}")
-		data = np.load(CACHE)
-		return data["train_x"], data["test_x"], data["val_x"]
+def load_path(path):
+	files = []
+	for folder in os.listdir(path):
+		sub_path = os.path.join(path, folder)
+		if not os.path.isdir(sub_path):
+			continue
+		for img in os.listdir(sub_path):
+			files.append(os.path.join(sub_path, img))
+	data = np.empty((len(files), 224, 224, 3), dtype=np.uint8)
+	n = 0
+	for img_path in files:
+		img_arr = cv2.imread(img_path)
+		if img_arr is not None:
+			data[n] = cv2.resize(img_arr, (224, 224))
+			n += 1
+	return data[:n]
+
+def get_data(path_needed):
+	print(f"Currently checking {path_needed}")
+
+	if os.path.exists(f"cache/{path_needed}"):
+		return np.load(f"cache/{path_needed}")
 
 	print(f"No cache found, reading images...")
-	train_path = "./archive/animals/train"
-	test_path = "./archive/animals/test"
-	val_path = "./archive/animals/validation"
+	path_arr = os.path.splitext(path_needed)[0]
+	path_arr = path_arr.split("_", 1)[0]
 
-	def load_path(path):
-		data = []
-		for folder in os.listdir(path):
-			sub_path = os.path.join(path, folder)
-			if not os.path.isdir(sub_path):
-				continue
-			for img in os.listdir(sub_path):
-				img_path = os.path.join(sub_path, img)
-				img_arr = cv2.imread(img_path)
-				if img_arr is not None:
-					img_arr = cv2.resize(img_arr, (224, 224))
-					data.append(img_arr)
-		return np.array(data)
+	x = load_path(f"archive/animals/{path_arr}")
+	np.save(f"cache/{path_needed}", x)
+	del x
 
-	train_x = load_path(train_path)
-	test_x = load_path(test_path)
-	val_x = load_path(val_path)
-
-	np.savez(CACHE, train_x=train_x, test_x=test_x, val_x=val_x)
-	print(f"Saved binary cache to {CACHE}")
-	print(f"Loaded: train={len(train_x)}, test={len(test_x)}, val={len(val_x)}")
-	return train_x, test_x, val_x
-
-
-if __name__ == "__main__":
-	get_data()
+	return np.load(f"cache/{path_needed}")
--- model.py
+++ model.py
@@ -1,5 +1,14 @@
 from database import get_data
 import cv2
 
-train_x, test_x, val_x = get_data()
+print("Loading data from cache...")
 
+train_x = get_data("train_x.npy")
+print(f"train={len(train_x)} ")
+del train_x
+test_x = get_data("test_x.npy")
+print(f"test={len(test_x)} ")
+del test_x
+val_x = get_data("val_x.npy")
+print(f"val={len(val_x)}")
+del val_x
